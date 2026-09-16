ea4fe03c691e47563e035f3cf9cc6ef911bd8c65
Duc Anh <emailcuaban@gmail.com>
2026-09-16 22:36:16 +0700

    upLab2

--- src/lab2.ts
+++ src/lab2.ts
@@ -1,27 +1,69 @@
-
-
-enum productRate{
+enum productRate {
     low = "thấp",
-    medium ="trung bình",
+    medium = "trung bình",
     hight = "cao",
-
 }
 
-const product:{name:string; rate: productRate; price: number} = {
 
+type Product = {
+    name: string;
+    rate: productRate;
+    sale: boolean;
+    price: number;
+};
+
+
+const product: Product = {
     name: "laptop",
     rate: productRate.low,
+    sale: false,
     price: 100
-
 };
-   console.log(product);
 
-   const listProduct=   [
+console.log(product);
+
+
+const listProduct: Product[] = [
     {
-     name: "laptop",
-    rate: productRate.low,
-    price: 100
+        name: "Laptop",
+        price: 3,
+        sale: false,
+        rate: productRate.medium
     },
+    {
+        name: "Điện thoại",
+        price: 4,
+        sale: false,
+        rate: productRate.medium
+    },
+    {
+        name: "Chuột",
+        price: 10,
+        sale: true,
+        rate: productRate.hight
+    },
+    {
+        name: "Bàn phím",
+        price: 3,
+        sale: false,
+        rate: productRate.medium
+    },
+    {
+        name: "Tai nghe",
+        price: 1,
+        sale: true,
+        rate: productRate.low
+    }
+];
 
-   ];
-  
+
+function themPhanTu(list: Product[]) {
+    return list.map((product) => {
+        return {
+            ...product,
+            description: product.price > 5 ? "Tốt" : "Bình thường"
+        };
+    });
+}
+
+console.log(themPhanTu(listProduct));
